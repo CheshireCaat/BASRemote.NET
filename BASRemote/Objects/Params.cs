fe24c817b291fe9fbf9b60a79c022f5b3d775840
CheshireCaat <likangt2012@gmail.com>
2020-02-06 23:46:42 +0300

    Update
    Add xml docs to Params class
    Restore Newtonsoft.Json dependency in nuspec
    Exclude Newtonsoft.Json from Costura

--- BASRemote/Objects/Params.cs
+++ BASRemote/Objects/Params.cs
@@ -2,8 +2,14 @@
 
 namespace BASRemote.Objects
 {
+    /// <summary>
+    ///     Class that contains arguments for functions and messages.
+    /// </summary>
     public sealed class Params : Dictionary<string, object>
     {
+        /// <summary>
+        ///     Create an empty instance of <see cref="Options" /> class.
+        /// </summary>
         public static Params Empty => new Params();
     }
 }
